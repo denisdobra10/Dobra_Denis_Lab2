3f57e2f2de0c082fd0ec4e2c641a1ff4663ae91a
Denis <Denis@DESKTOP-P1BQ5TB>
2022-10-30 18:26:00 +0200

    Add project files.

---
+++ Data/Dobra_Denis_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Dobra_Denis_Lab2.Models;
+
+namespace Dobra_Denis_Lab2.Data
+{
+    public class Dobra_Denis_Lab2Context : DbContext
+    {
+        public Dobra_Denis_Lab2Context (DbContextOptions<Dobra_Denis_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Dobra_Denis_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
